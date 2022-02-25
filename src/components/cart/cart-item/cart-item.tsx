6fcaba6da12338ab76214dcfff6b2566c19935ab
Azarak <abbatd@gmail.com>
2022-02-25 22:22:48 +1000

    add props to CartItem

--- src/components/cart/cart-item/cart-item.tsx
+++ src/components/cart/cart-item/cart-item.tsx
@@ -1,22 +1,37 @@
-export default function CartItem(): JSX.Element {
+import { Product } from '../../../types/product';
+
+export default function CartItem({item}: {item: Product}): JSX.Element {
+  const {
+    id,
+    name,
+    price,
+    quantity,
+  } = item;
+
   return (
     <div className="cart-item">
       <button className="cart-item__close-button" type="button" aria-label="Delete">
         <span className="cart-item__close-button-icon"></span>
       </button>
       <div className="cart-item__image">
-        <img src="img/bag.png" width="100" height="100" alt="Product name" />
+        <img src="img/bag.png" width="100" height="100" alt={`${name}`} />
       </div>
       <div className="cart-item__info">
-        <p className="cart-item__info__title">Product Title</p>
+        <p className="cart-item__info__title">{name}</p>
       </div>
-      <div className="cart-item__price">1 000 $</div>
+      <div className="cart-item__price">{price} $</div>
       <div className="quantity cart-item__quantity">
         <button className="quantity__button" aria-label="Decrease quantity">-</button>
-        <input className="quantity__input" type="number" placeholder="1" id="2-count" name="2-count" />
+        <input
+          className="quantity__input"
+          type="number"
+          placeholder="1"
+          id={`${id}`}
+          name={`${id}-count`}
+        />
         <button className="quantity__button" aria-label="Increase quantity">+</button>
       </div>
-      <div className="cart-item__price-total">1 000 ₽</div>
+      <div className="cart-item__price-total">{price * quantity} $</div>
     </div>
   );  
 }
--- src/components/cart/cart.tsx
+++ src/components/cart/cart.tsx
@@ -1,4 +1,26 @@
 import CartItem from './cart-item/cart-item'
+import { Product } from '../../types/product';
+
+const mockItems: Product[] = [
+  {
+    id: 1,
+    name: 'Some name 1',
+    price: 111000,
+    quantity: 1,
+  },
+  {
+    id: 2,
+    name: 'Some name 2',
+    price: 300,
+    quantity: 3,
+  },
+  {
+    id: 3,
+    name: 'Some name 3',
+    price: 200,
+    quantity: 2,
+  },
+];
 
 export default function Cart(): JSX.Element {
   return (
@@ -12,7 +34,11 @@ export default function Cart(): JSX.Element {
         </div>
       </div>
       <div className="cart">
-        <CartItem />
+        {
+          mockItems.length
+            ? mockItems.map((item) => <CartItem item={item} />)
+            : <p><i>Your cart is empty</i></p>
+        }
         <div className="cart__footer">
           <div className="cart__total-info">
             <p className="cart__total-item">
