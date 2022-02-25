b784b9ff539e5f6daaaac9879e2f55e7f647164b
Azarak <abbatd@gmail.com>
2022-02-25 23:13:24 +1000

    add mock server

--- src/types/actions.ts
+++ src/types/actions.ts
@@ -1,3 +1,14 @@
+import {
+  Action,
+  ThunkAction,
+  ThunkDispatch
+} from '@reduxjs/toolkit';
+import { AxiosInstance } from 'axios';
+import { State } from './state';
+
 export enum ActionType {
   LoadProducts = 'data/loadProducts',
 };
+
+export type ThunkActionResult<R = Promise<void>> = ThunkAction<R, State, AxiosInstance, Action>;
+export type ThunkAppDispatch = ThunkDispatch<State, AxiosInstance, Action>;
